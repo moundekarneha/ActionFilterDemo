d717f69a3e0822c805a6c5d8c7fc75ac3fd792ba
HP <HP@DESKTOP-G4J7E8F>
2022-08-26 14:59:36 +0530

    Add project files.

---
+++ ActionFilterDemo/Filters/CustomActionFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc.Filters;
+using System.Web.Mvc;
+using System.Diagnostics;
+
+
+namespace ActionFilterDemo.Filters
+{ 
+    public class CustomActionFilter : FilterAttribute,IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            Debug.WriteLine("hello from action filter - OnActionExecuted");
+            filterContext.Controller.ViewBag.Message = "We are from action filter";
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Debug.WriteLine("hello from action filter - OnActionExecuting");
+        }
+    }
+}
